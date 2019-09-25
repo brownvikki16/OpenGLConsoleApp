306a5ca84bdbe33a1b1651e00bca1cffa49e8f04
Vikki Brown <brownvikki16@gmail.com>
2019-09-24 17:00:36 -0700

    Successful color interpolation Successful Indexed Draw Managed to create a rotating 3D triangle with the RGB values interpolating based on position

--- main.cpp
+++ main.cpp
@@ -11,55 +11,85 @@
 
  //window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+//convert to Radians
+const float toRadians = 3.14159265f / 180.0f;
+
 
 //variables
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;   
 float triMaxOffset = 0.7f;  
 float triIncrement = 0.0005f;  
 
+float curAngle = 0.0f;
+
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
+
 //Vertex Shader
 //note the input variables will change often but the uniform 
 //variables will not change and will be applied to every vertex
-static const char* vShader = "								 \n\
-#version 330												 \n\
-															 \n\
+static const char* vShader = 
+"								\n\
+#version 330												\n\
+															\n\
 layout (location = 0) in vec3 pos;							\n\
 															\n\
+out vec4 vertexColor;										\n\
+															\n\
 uniform mat4 model;											\n\
-															 \n\
-void main()													 \n\
-{															 \n\
-   gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0); \n\
+															\n\
+void main()													\n\
+{															\n\
+   gl_Position = model * vec4(pos, 1.0);					\n\
+   vertexColor = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);		\n\
 }";
 
 
 //Fragment Shader
 static const char* fShader =
 "#version 330                                               \n\
+in vec4 vertexColor;										\n\
 out vec4 color;                                             \n\
                                                             \n\
 void main()                                                 \n\
 {                                                           \n\
-  color = vec4(1.0, 0.0, 0.0, 1.0);                         \n\
+  color = vertexColor;				                        \n\
 }";                                                          
 
 
 void CreateTriangle()
 {
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
 	//array of triangle vertex points
 	GLfloat vertices[] = {
-		-1.0f, -1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f
+		-1.0f, -1.0f, 0.0f, //0
+		0.0f, -1.0f, 1.0f, //1
+		1.0f, -1.0f, 0.0f, //2
+		0.0f, 1.0f, 0.0f, //3
 	};
 
 	//creating VAO ID (how many, where the ID is)
 	glGenVertexArrays(1, &VAO);
 	//bind VAO with that ID
 	glBindVertexArray(VAO);
+
+	//create IBO buffer
+	glGenBuffers(1, &IBO);
+	//bind IBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	//define buffer data
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//create a VBO ID (how many, where the ID is)
 	glGenBuffers(1, &VBO);
@@ -73,11 +103,14 @@ void CreateTriangle()
 	//enable vertex array
 	glEnableVertexAttribArray(0);
 
+	//Unbind VAO
+	glBindVertexArray(0);
+
 	//Unbind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	//Unbind VAO
-	glBindVertexArray(0);
+	//unbind IBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 //
@@ -201,6 +234,9 @@ int main()
 		return 1;
 	}
 
+	//gl depth test to determine what is in front and behind
+	glEnable(GL_DEPTH_TEST);
+
 	//create viewport with size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
@@ -228,11 +264,31 @@ int main()
 			direction = !direction;  //swap bool value
 		}
 
+		curAngle += 0.01f;
+		if (curAngle >=360)
+		{
+			curAngle -= 360;
+		}
+
+		if (sizeDirection)
+		{
+			curSize += 0.001f;
+		}
+		else
+		{
+			curSize -= 0.001f;
+		}
+
+		if (curSize>= maxSize || curSize <= minSize)
+		{
+			sizeDirection = !sizeDirection;
+		}
+
 		//clear the window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //clears out all drawn things
 		//each pixel has a lot of data in it. this line of code below
 		//is clearing the color buffer bit data
-		glClear(GL_COLOR_BUFFER_BIT); 
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
 		//actual drawing, will use shader with the current shader ID
 		glUseProgram(shader);
@@ -240,14 +296,24 @@ int main()
 		//4x4 matrix, automatically set to an identity matrix
 		glm::mat4 model(1.0f);
 		//take identity matrix and apply translation to it. the x value will change to triOffset
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		//rotate model by 45 degrees on the Z axis
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 		//set uniform value to desired variable (triOffset)void
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		
 		//bind vertex array
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//bind IBO
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+		
+		//unbind IBO
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		
 		//unbind vertex array
 		glBindVertexArray(0);
 		
